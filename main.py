1b6cd4120124d5aace20e12da9221f5d7216f19c
Nuttasit Tinmas <9potae.work@gmail.com>
2023-12-23 14:57:03 +0700

    create a-z button

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 import kivy
 
 from kivy.app import App
@@ -7,13 +9,37 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 
 class ButtonsLayout(GridLayout):
-    pass
+    instance = []
+
+    def __init__(self, **kwargs):
+        super(ButtonsLayout, self).__init__(**kwargs)
+
+        ButtonsLayout.instance.append(self)
+
+        self.rows = 2
+        self.cols = 13
+
+        self.buttons = {}
+
+        # Creating the buttons.
+        self.create_buttons()
+
+    def create_buttons(self):
+        # Creating buttons for all the alphabets.
+        for alphabet in string.ascii_uppercase:
+            # Creating button.
+            button = Button(text=alphabet)
+
+            self.add_widget(button)
+
+            self.buttons[alphabet] = button
 
 class MyRoot(BoxLayout):
     pass
 
 class Hangman(App):
-    pass
+    def build(self):
+        return ButtonsLayout()
 
 if __name__ == "__main__":
     Hangman().run()
