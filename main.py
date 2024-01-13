3e4b6a582d99ff4e4fede48827f574b8380a066d
DatboisouL <9potae.work@gmail.com>
2024-01-13 12:55:35 +0700

    The complete version of the game

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.audio import SoundLoader
 from kivy.uix.popup import Popup
-from words import Words
+from words import Words #Assuming 'Words' is a variable with a list of words.
 
+#Define a class for the layout containing buttons
 class ButtonsLayout(GridLayout):
-    instance = []
-    background_music = SoundLoader.load('sound/game_music1.mp3')
-    button_sound = SoundLoader.load('sound/button_sound.mp3')
+    instance = [] #List to store instances of ButtonsLayout
+    background_music = SoundLoader.load('sound/game_music1.mp3')  #Load background music
+    button_sound = SoundLoader.load('sound/button_sound.mp3') #Load button press sound
 
     @classmethod
     def play_background_music(cls):
@@ -41,6 +42,7 @@ class ButtonsLayout(GridLayout):
         self.buttons = {}
         self.create_buttons()
 
+    #Create letter buttons and a hint button
     def create_buttons(self):
         for alphabet in string.ascii_uppercase:
             button = Button(
@@ -65,9 +67,11 @@ class ButtonsLayout(GridLayout):
         self.add_widget(hint_button)
         self.buttons['Hint'] = hint_button
 
+    #setting button press
     def on_setting_button_press(self, instance):
         print("Setting button pressed")
 
+    #Display a popup with settings options
     def show_settings_popup(self, instance):
         content = BoxLayout(orientation='vertical', spacing=10)
         change_song_button1 = Button(text='Change Song 1', font_size=24, on_press=self.change_song1)
@@ -81,6 +85,7 @@ class ButtonsLayout(GridLayout):
         popup = Popup(title='Settings', content=content, size_hint=(None, None), size=(400, 300))
         popup.open()
 
+    #Change the background music to Song 1
     def change_song1(self, instance=None):
         print("Changing to Song 1")
         if self.background_music:
@@ -90,6 +95,7 @@ class ButtonsLayout(GridLayout):
             self.background_music.volume = 0.125
             self.background_music.play()
 
+    #Change the background music to Song 2
     def change_song2(self, instance=None):
 
         print("Changing to Song 2")
@@ -100,6 +106,7 @@ class ButtonsLayout(GridLayout):
             self.background_music.volume = 0.125
             self.background_music.play()
 
+    #Change the background music to Song 3
     def change_song3(self, instance=None):
         print("Changing to Song 3")
         if self.background_music:
@@ -109,16 +116,20 @@ class ButtonsLayout(GridLayout):
             self.background_music.volume = 0.125
             self.background_music.play()
 
+    #exit button press
     def on_exit_button_press(self, instance):
         print("Exit button pressed")
         App.get_running_app().stop()
 
+    #letter buttons press, plays a button press sound
     def on_button_press(self, instance):
         self.button_sound.play()
 
+    #hint button press
     def on_hint_button_press(self, instance):
         self.show_hint()
 
+#Define the root layout class
 class MyRoot(BoxLayout):
     ERRORS = StringProperty()
     HANGMAN_IMG = StringProperty()
@@ -133,10 +144,12 @@ class MyRoot(BoxLayout):
         self.configure_buttons()
         self.start_game()
 
+    #Property to check if the player has won
     @property
     def won(self):
         return all(alphabet in self.GUESSES for alphabet in self.RANDOM_WORD)
-
+    
+    #update the displayed word based on guesses
     def update_word_display(self):
         WORD_DISPLAY = []
         for alphabet in self.RANDOM_WORD:
@@ -146,10 +159,12 @@ class MyRoot(BoxLayout):
                 WORD_DISPLAY.append("_")
         self.WORD_DISPLAY = " ".join(WORD_DISPLAY)
 
+    #letter buttons press
     def btn_press(self, widget):
         widget.disabled = True
         self.GUESSES.append(widget.text)
 
+        #Check if the guessed letter is in the word
         if widget.text in self.RANDOM_WORD:
             self.update_word_display()
             if self.won:
@@ -157,6 +172,8 @@ class MyRoot(BoxLayout):
                     button.disabled = True
                 self.HANGMAN_IMG = "images/vic.jpg"
                 self.GAME_MSG = "You Won!!!!"
+
+        #incorrect guess       
         else:
             self.ERRORS = str(int(self.ERRORS) + 1)
             self.HANGMAN_IMG = "images/hangman" + self.ERRORS + ".png"
@@ -167,6 +184,7 @@ class MyRoot(BoxLayout):
                 self.WORD_DISPLAY = self.RANDOM_WORD
                 ButtonsLayout.stop_background_music()
 
+    #Configure letter buttons and the settings button
     def configure_buttons(self):
         for button in self.buttons_layout.buttons.values():
             button.on_press = lambda btn=button: self.btn_press(btn)
@@ -174,6 +192,7 @@ class MyRoot(BoxLayout):
         if setting_button:
             setting_button.bind(on_press=lambda btn=setting_button: self.on_setting_button_press())
 
+    #Display a hint by revealing the letter
     def show_hint(self):
         if self.RANDOM_WORD and not self.won:
             unrevealed_letters = [letter for letter in self.RANDOM_WORD if letter not in self.GUESSES]
@@ -182,6 +201,7 @@ class MyRoot(BoxLayout):
                 self.GUESSES.append(hint_letter)
                 self.update_word_display()
 
+    #to start a new game or restart game
     def start_game(self):
         self.RANDOM_WORD = random.choice(Words)
         self.GUESSES.clear()
@@ -193,13 +213,16 @@ class MyRoot(BoxLayout):
             button.disabled = False
         ButtonsLayout.play_background_music()
 
+    #setting button press
     def on_setting_button_press(self):
         print("Setting button pressed")
         self.buttons_layout.show_settings_popup(None)
 
+#main application class
 class Hangman(App):
     def build(self):
         return MyRoot()
 
+#Run the application
 if __name__ == "__main__":
     Hangman().run()
